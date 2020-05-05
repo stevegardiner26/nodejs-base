3a8094120cdbffd74ecd8ea79dcbd5a622b6fe75
Steeeve <stevegardiner26@aol.com>
2020-05-05 17:06:22 -0400

    Added Basic Internet Homepage

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var port = 3000;
 var express = require("express");
 var app = express();
+var path = require('path');
 
 app.listen(port, () => {
     console.log("Server running on port " + port);
 });
 
 app.get("/", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+    // res.json({});
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
