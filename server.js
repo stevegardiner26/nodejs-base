8c408480d0ef731b62efa59de8e63cb0c283f7b8
Steeeve <stevegardiner26@aol.com>
2020-05-14 19:29:12 -0400

    Added Notation for Server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.listen(port, () => {
     console.log("Server running on port " + port);
 });
 
+// All of the sites and routes should be stored in the DB in meta data, will require an absurd amount of time to load the sites though
+
 app.get("/", (req, res, next) => {
     // res.json({});
     res.sendFile(path.join(__dirname + '/index.html'));
